c3c8267fbc611480c1a3eef998ddc2e36072c4ee
manjotssd <80041617+manjotssd@users.noreply.github.com>
2022-02-17 18:07:54 +0530

    minor fixes

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,19 +1,53 @@
 import React, { useEffect, useRef } from 'react';
-import { Button, View, FlatList, ActivityIndicator, Text,Animated, StyleSheet } from 'react-native';
+import { Button, View, FlatList, ActivityIndicator, Text, Animated, StyleSheet } from 'react-native';
 import { PanResponder } from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
 import { colors } from '../../Constants/theme';
-export default  ({ navigation }) => {
+export default ({ navigation }) => {
   const pan = useRef(new Animated.ValueXY()).current;
-  const rot = useRef(new Animated.Value()).current;
-  let firstProgressLayerStyle,percent = 10;
+  const rotation = useRef(new Animated.Value(0)).current;
+  const offsetOpacity = useRef(new Animated.Value(0)).current;
+  const startAngle = 0;
+  const endAngle = 360;
+  let pts;
+
+
+ const getDeg = (cX, cY, pts) => {
+    const x = cX - pts.x;
+    const y = cY - pts.y;
+    let deg = Math.atan(y / x) * 180 / Math.PI;
+    if ((x < 0 && y >= 0) || (x < 0 && y < 0)) {
+      deg += 90;
+    } else {
+      deg += 270;
+    }
+    let finalDeg = Math.min(Math.max(startAngle, deg), endAngle);
+    return finalDeg;
+  };
+
+  pan.addListener((obj) => setOpacity(obj));
+  const setOpacity = (obj) => {
+    //console.log("anim", obj.y);
+    let degg = getDeg(obj.x,obj.y,pts);
+    console.log("deg:",degg);
+    rotation.setValue(degg);
+     if (degg > 180) {
+      offsetOpacity.setValue(0);
+     // console.log("in if", obj.y);
+
+    } else {
+    //  console.log("in else", obj.y);
+      offsetOpacity.setValue(1);
+    }
+  }
+  let firstProgressLayerStyle, percent = 10;
   const panResponder = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
       onPanResponderGrant: () => {
         pan.setOffset({
           x: pan.x._value,
-          y: pan.y._value
+          y: pan.y._value,
         });
       },
       onPanResponderMove: Animated.event(
@@ -24,58 +58,85 @@ export default  ({ navigation }) => {
       ),
       onPanResponderRelease: () => {
         console.log(pan);
+
         pan.flattenOffset();
       }
     })
   ).current;
 
   const renderThirdLayer = (percent) => {
-    if(percent > 50){
+    if (percent > 50) {
       /**
       * Third layer circle default is 45 degrees, so by default it occupies the right half semicircle.
       * Since first 50 percent is already taken care  by second layer circle, hence we subtract it
       * before passing to the propStyle function
       **/
-      return <Animated.View style={[styles.secondProgressLayer,propStyle((percent - 50), 45) ]}>
+      return <Animated.View style={[styles.secondProgressLayer, propStyle((percent - 50), 45)]}>
 
       </Animated.View>
-    }else{
+    } else {
       return <Animated.View style={styles.offsetLayer}></Animated.View>
     }
   }
   const propStyle = (percent, base_degrees) => {
+    console.log("percent:", percent);
     const rotateBy = base_degrees + (percent * 3.6);
     return {
-      transform:[{rotateZ: `${rotateBy}deg`}]
+      transform: [{ rotateZ: `${rotateBy}deg` }]
     };
   }
 
-  if(percent > 50){
-      firstProgressLayerStyle = propStyle(50, -135);
-  }else {
+  if (percent > 50) {
+    firstProgressLayerStyle = propStyle(50, -135);
+  } else {
     firstProgressLayerStyle = propStyle(percent, -135);
   }
-console.log(pan.x._value);
-    return (
-      <Animated.View style={{...styles.container2,
-        transform: [{ translateX: pan.x }, { translateY: pan.y }]}}
+  console.log(pan.x._value);
+  return (
+    <Animated.View style={{
+      ...styles.container2,
+      //  transform: [{ rotateZ: pan.x.interpolate({
+      //   inputRange: [0, 700],
+      //   outputRange: ["0deg", "360deg"]
+      // }) }]
+    }}
+onLayout={event =>{
+  const layout = event.nativeEvent.layout;
+  pts = {
+    x: layout.x + layout.width / 2,
+    y: layout.y + layout.height / 2
+  };
 
+  // console.log('height:', layout.height);
+  // console.log('width:', layout.width);
+  // console.log('x:', layout.x);
+  // console.log('y:', layout.y);
+}}
       {...panResponder.panHandlers}
-      >
-        <Animated.View style={[styles.firstProgressLayer,firstProgressLayerStyle]} >
-       
+    >
+      <Animated.View style={{
+        ...styles.firstProgressLayer,
+        transform: [{
+          rotateZ: rotation.interpolate({
+            inputRange: [0, 360],
+            outputRange: ["-135deg", "360deg"],
+            extrapolate: "clamp"
+          })
+        }]
+      }} >
+      </Animated.View>
+      <Animated.View style={{
+        ...styles.offsetLayer, opacity: offsetOpacity, transform: [{ rotateZ: '-135deg' }]
+      }} >
+      </Animated.View>
+      <Animated.View style={{
+        ...styles.secondProgressLayer, opacity: offsetOpacity == 0  ?1:0, transform: [{ rotateZ: '45deg' }]
+      }} >
+      </Animated.View>
+    </Animated.View>
 
-       
 
-        </Animated.View>
-        {renderThirdLayer(pan.y.interpolate({
-  inputRange: [0, 400],
-  outputRange: [0, 100],
-  extrapolate: 'clamp'
-}))}</Animated.View>
-     
-    
-    );
+  );
 }
 const styles = StyleSheet.create({
   container2: {
@@ -85,7 +146,8 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     borderColor: 'grey',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    left:100
   },
   firstProgressLayer: {
     width: 200,
@@ -97,9 +159,9 @@ const styles = StyleSheet.create({
     borderBottomColor: 'transparent',
     borderRightColor: '#3498db',
     borderTopColor: '#3498db',
-    transform:[{rotateZ: '-135deg'}]
+    transform: [{ rotateZ: '-135deg' }]
   },
-  secondProgressLayer:{
+  secondProgressLayer: {
     width: 200,
     height: 200,
     position: 'absolute',
@@ -108,8 +170,8 @@ const styles = StyleSheet.create({
     borderLeftColor: 'transparent',
     borderBottomColor: 'transparent',
     borderRightColor: '#3498db',
-    borderTopColor:'#3498db',
-    transform: [{rotateZ: '45deg'}]
+    borderTopColor: '#3498db',
+    transform: [{ rotateZ: '45deg' }]
   },
   offsetLayer: {
     width: 200,
@@ -119,8 +181,8 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     borderLeftColor: 'transparent',
     borderBottomColor: 'transparent',
-    borderRightColor: 'grey',
-    borderTopColor: 'grey',
-    transform:[{rotateZ: '-135deg'}]
+    borderRightColor: 'green',
+    borderTopColor: 'green',
+    transform: [{ rotateZ: '-135deg' }]
   }
 });
